080907f6d5301a65416b5aa3f2e0b78a78e5f125
Julian David Celis Giraldo <103604347+JulianCelisCreator@users.noreply.github.com>
2024-09-14 18:57:40 -0500

    Update cli.py

--- ArcadeMachine/cli.py
+++ ArcadeMachine/cli.py
@@ -1,29 +1,36 @@
 """
-This module has class definition both abstract and concrete
-for calculators.
+The code in the given module is an interactive program that allows users to interact
+with an arcade machine catalog. It starts by prompting the user to select the type 
+of arcade machine (modern or retro). 
+Then, it displays the available games in the catalog. The user can choose to add 
+games to the machine. Finally, the program asks for customer information 
+(name, address, phone number)
+and completes the purchase. The module also defines a `Game` class and creates 
+instances of it to represent the available games in the catalog.
 
 Author: Julian David Celis Giraldo <jdcelisg@udistrital.edu.co>
 
-This file is part of PyCalculator-UD.
+This file is part of ArcadeMachine.
 
-PyCalculator-UD is free software: you can redistribute it and/or 
+ArcadeMachine is free software: you can redistribute it and/or 
 modify it under the terms of the GNU General Public License as 
 published by the Free Software Foundation, either version 3 of 
 the License, or (at your option) any later version.
 
-PyCalculator-UD is distributed in the hope that it will be useful, 
+ArcadeMAchine is distributed in the hope that it will be useful, 
 but WITHOUT ANY WARRANTY; without even the implied warranty of 
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
 General Public License for more details.
 
 You should have received a copy of the GNU General Public License 
-along with PyCalculator-UD. If not, see <https://www.gnu.org/licenses/>. 
+along with ArcadeMachine If not, see <https://www.gnu.org/licenses/>. 
 """
 
 # Google Doc Python: python documentation style guide
 # Doc String
 from ArcadeMachine import ArcadeCatalog, Game
-    
+
+
 def main():
     """
     Main function to interact with the arcade catalog.
@@ -31,14 +38,16 @@ def main():
     view available games, add games to the machine,
     and complete the purchase by entering customer details.
     """
-    
+
     catalog = ArcadeCatalog()
 
     print("Welcome to the Arcade Machine Catalog.")
     while True:
         try:
             # Prompt user to select the type of arcade machine
-            machine_type = int(input("\nSelect the machine type: 1. Modern, 2. Retro: "))
+            machine_type = int(
+                input("\nSelect the machine type: 1. Modern, 2. Retro: ")
+            )
             break
         except ValueError:
             print("Invalid input. Please enter 1 or 2.")
